74ca867d21a1c30b7f1109ece5c6c13227bdcf10
sriramulavenkatesh <venkat7779@hotmail.com>
2019-04-19 20:49:55 +0530

    commit 2049

---
+++ com.testCuke/src/test/java/com/lessons/one/ResizeTest.java
@@ -0,0 +1,43 @@
+package com.lessons.one;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class ResizeTest {
+	WebDriver driver;
+	
+	
+	public void sample() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "D:\\Automation Stuff\\Chrome driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://jqueryui.com/resizable/");
+		driver.switchTo().frame(0);
+		WebElement eleResizable = driver.findElement(By.xpath("//div[@id='resizable']"));
+		WebElement resizableRight = driver.findElement(By.xpath("//div[@class='ui-resizable-handle ui-resizable-e']"));
+		Actions action = new Actions(driver);
+		action.moveToElement(resizableRight).clickAndHold(resizableRight).moveByOffset(100, 50).build().perform();
+		Thread.sleep(10000);
+	}
+	@Test
+	public void sampleTwo() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "D:\\Automation Stuff\\Chrome driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://codepen.io/jasongardner/pen/QNOXym");
+		WebElement resizerOne = driver.findElement(By.xpath("//div[@id='resizer']"));
+		Actions action = new Actions(driver);
+		action.moveToElement(resizerOne).clickAndHold(resizerOne).moveByOffset(0, -150).release(resizerOne).build().perform();
+		driver.switchTo().frame(0);
+		WebElement tableEleOne = driver.findElement(By.xpath("//table[1]//th[3]"));
+		int y = tableEleOne.getLocation().getY();
+		Thread.sleep(5000);
+		action.moveToElement(tableEleOne).clickAndHold(tableEleOne).moveByOffset(80, y).release().build().perform();
+		Thread.sleep(5000);
+		
+		
+	}
+
+}
