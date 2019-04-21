da31cc3649f2eb9187fae9d7c42e42752b066c99
sriramulavenkatesh <venkat7779@hotmail.com>
2019-04-21 18:22:18 +0530

    commit 1822

---
+++ com.testCuke/src/test/java/com/lessons/one/SwitchExample.java
@@ -0,0 +1,31 @@
+package com.lessons.one;
+
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
+public class SwitchExample {
+	
+	@Test
+	public void sample() {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a string = ");
+		String sa = s.nextLine();
+		switch(sa) {
+		
+		case "venkat":
+			System.out.println("you are venkat");
+			break;
+		case "sriram":
+			System.out.println("you are sriram");
+			break;
+		 default:
+			System.out.println("you are none");
+			break;
+		
+		}
+		
+	}
+
+}
